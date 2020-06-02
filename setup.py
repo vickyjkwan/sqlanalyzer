81dafbde09490cbc0a0f2071b276a1c3f184a4c1
mathilda0902 <vicky.kwan@mapbox.com>
2020-06-02 12:28:51 -0700

    v.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='sqlanalyzer',
-      version='0.2',
+      version='0.21',
       description='A tool to parse and analyze the structure for Postgres sql queries.',
       classifiers=[
         'License :: OSI Approved :: MIT License',
