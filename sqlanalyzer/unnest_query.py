fab6945d6683760ca352c5c4d76c1b5f3aa4a663
mathilda0902 <vicky.kwan@mapbox.com>
2020-06-08 12:13:35 -0700

    fixed regex

--- sqlanalyzer/unnest_query.py
+++ sqlanalyzer/unnest_query.py
@@ -73,7 +73,7 @@ def parse_sub_query(query_list, sub_query_pos):
                 query.append(' '.join(alias_list_rev[::-1]).rstrip(r'\)').lstrip(' '))
 
             else:
-                alias_list_rev[0] = alias_list_rev[0].rstrip('\)')
+                alias_list_rev[0] = alias_list_rev[0].rstrip(r'\)')
                 alias = 'none'
                 query.append(' '.join(alias_list_rev[::-1]))
 
@@ -81,7 +81,7 @@ def parse_sub_query(query_list, sub_query_pos):
             query.append(' '.join(alias.split(' ')[:-1]).rstrip(r'\)').lstrip(' '))
             alias = alias.split(' ')[-1]
 
-        sub_query[alias] = ' '.join(query).lstrip(' \(').lstrip(' FROM')
+        sub_query[alias] = ' '.join(query).lstrip(r' \(').lstrip(' FROM')
         
     return sub_query
 
@@ -103,53 +103,13 @@ def has_child(formatted_query):
 
 
 def main():
-    # query = """SELECT sfdc_accounts.platform, sfdc_accounts.mobile_os, sfdc_accounts.service_metadata,
-    #         sfdc_cases.account, sfdc_cases.num_requests, sfdc_cases.owner
-    #         FROM sfdc.accounts sfdc_accounts
-    #         LEFT JOIN (SELECT MAX(dt) FROM (SELECT dt FROM sfdc.oppty) sfdc_oppty LEFT JOIN (SELECT dt FROM sfdc.cases) sfdc_cases ON sfdc_oppty.dt = sfdc_cases.dt) 
-    #         AS sfdc_cases_oppty ON sfdc_cases_oppty.dt = sfdc_accounts.dt
-    #         WHERE sfdc_cases_oppty.dt > '2020-04-03' AND sfdc_cases_oppty.dt < '2020-05-04' ORDER BY 1 GROUP BY 3 LIMIT 20
-    #         """
-
-    query = """SELECT *
-   FROM
-     (SELECT a.*,
-             b.*,
-             c.*,
-             d.*
-      FROM
-        (SELECT DISTINCT anonymous_id,
-                         user_id
-         FROM mapbox_customer_data.segment_identifies
-         WHERE dt >= '2018-07-01'
-           AND anonymous_id IS NOT NULL
-           AND user_id IS NOT NULL ) a
-      LEFT JOIN
-        (SELECT id,
-                email,
-                created
-         FROM mapbox_customer_data.accounts
-         WHERE cast(dt AS DATE) = CURRENT_DATE - INTERVAL '1' DAY ) b ON a.user_id = b.id
-      LEFT JOIN
-        (SELECT anonymous_id AS anon_id_ad,
-                context_campaign_name,
-                min(TIMESTAMP) AS min_exposure
-         FROM mapbox_customer_data.segment_pages
-         WHERE dt >= '2018-07-01'
-           AND context_campaign_name IS NOT NULL
-         GROUP BY 1,
-                  2) c ON a.anonymous_id = c.anon_id_ad
-      LEFT JOIN
-        (SELECT DISTINCT anonymous_id AS anon_id_event,
-                         original_timestamp,
-                         event,
-                         context_traits_email
-         FROM mapbox_customer_data.segment_tracks
-         WHERE dt >= '2018-07-01'
-           AND event LIKE 'submitted_%form'
-           AND context_traits_email IS NOT NULL ) d ON a.anonymous_id = d.anon_id_event)
-   WHERE context_campaign_name IS NOT NULL 
-    """
+    query = """SELECT sfdc_accounts.platform, sfdc_accounts.mobile_os, sfdc_accounts.service_metadata,
+            sfdc_cases.account, sfdc_cases.num_requests, sfdc_cases.owner
+            FROM sfdc.accounts sfdc_accounts
+            LEFT JOIN (SELECT MAX(dt) FROM (SELECT dt FROM sfdc.oppty) sfdc_oppty LEFT JOIN (SELECT dt FROM sfdc.cases) sfdc_cases ON sfdc_oppty.dt = sfdc_cases.dt) 
+            AS sfdc_cases_oppty ON sfdc_cases_oppty.dt = sfdc_accounts.dt
+            WHERE sfdc_cases_oppty.dt > '2020-04-03' AND sfdc_cases_oppty.dt < '2020-05-04' ORDER BY 1 GROUP BY 3 LIMIT 20
+            """
 
     formatter = column_parser.Parser(query)
     formatted_query = formatter.format_query(query)
