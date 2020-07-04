6b3a5a0a81c9f74f61725d5f73267d2097a5df99
mathilda0902 <vicky.kwan@mapbox.com>
2020-07-04 15:44:48 -0700

    bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='sqlanalyzer',
-      version='0.303',
+      version='0.31',
       description='A tool to parse and analyze the structure for Postgres sql queries.',
       classifiers=[
         'License :: OSI Approved :: MIT License',
